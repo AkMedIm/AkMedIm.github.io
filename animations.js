6fc5a4a707d72945c665c31bc7e8c63bc2e7674d
Simo <25636694+AkMedIm@users.noreply.github.com>
2017-08-13 19:39:10 +0100

    Create animations.js

---
+++ animations.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".carousel-img").addClass("animated bounce");
+  $(".carousel-img1").addClass("animated shake");
+  $(".carousel-p").addClass("animated bounce");
+  $(".carousel-title").addClass("animated bounce");
+  $("#uparrow").addClass("animated shake");
+});
